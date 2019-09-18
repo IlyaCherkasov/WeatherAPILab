8f73481510fb3ee91338dac50258ae46820e33c5
Cherkasik <ilyakopeysk@gmail.com>
2019-09-18 22:21:32 +0300

    The weather API is done

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WeatherAPILab
 Web-programming lab based on working with weather API, Less and Mustache
+
+To start working with this project, you need to install npm and run "npm install" in the root of the project.
+You need to change styles in .less file. After you did everything you want, you need to run "npm run build" and launch index.html
---
+++ script.js
@@ -0,0 +1,26 @@
+const APIKey = 'e34c51f8b71bca2945c6901e59dc5b69';
+let template = `
+    {{#weather}}
+        <p>The weather is {{description}}</p>
+    {{/weather}}
+    {{#main}}
+        <p>The temperature is {{temp}} in Kelvins</p>
+        <p>The humidity is {{humidity}}%</p>
+        <p>The pressure is {{pressure}} hPa</p>
+    {{/main}}
+    {{#wind}}
+        <p>The wind is {{speed}} meters per hour</p>
+    {{/wind}}
+`;
+async function handleClick() {
+    let inputValue = document.getElementById('city').value;
+    const outputField = document.getElementById('weather');
+    let weather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&APPID=${APIKey}`);
+    if (weather.ok) {
+        const jsonWeather = await weather.json();
+        const html = Mustache.to_html(template, jsonWeather);
+        outputField.innerHTML = html;
+    } else {
+        alert(`Error while requesting API: ${weather.status} ${weather.statusText}. Try another one, please.`);
+    }
+}
